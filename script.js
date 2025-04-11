a0892a490863b668a29fe6ad02abece85ee5ce29
nikholas117 <nikholas123@hotmail.com>
2025-04-10 20:32:34 -0400

    added a changed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function generateExcuse() {
   let who = ["the dog", "my grandma", "his turtle", "my bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
   let what = ["ate", "pissed on", "crushed", "broke"];
   let when = [
     "before the class",
@@ -12,8 +13,10 @@ function generateExcuse() {
   let randomWho = who[Math.floor(Math.random() * who.length)];
   let randomWhat = what[Math.floor(Math.random() * what.length)];
   let randomWhen = when[Math.floor(Math.random() * when.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
 
-  let excuse = randomWho + " " + randomWhat + " " + randomWhen;
+  let excuse =
+    randomWho + " " + randomWhat + " " + randomWhen + " " + randomAction;
 
   document.getElementById("excuse").innerHTML = excuse;
 }
